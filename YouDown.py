4770b7ce39fd5def94982e2b93afdfe893769a3e
LeNonante <aurelien.musset54@gmail.com>
2024-08-21 14:38:10 +0200

    Nouvelle UI + Récuperation des infos de chaque vidéo lorsque qu'on apppuie sur le bouton DL A faire :  Telecharger chaque vidéo et afficher avancement dans UI

--- YouDown.py
+++ YouDown.py
@@ -3,10 +3,7 @@ from tkinter import *
 from tkinter import filedialog, ttk
 import customtkinter
 from PIL import Image
-
-
-import asyncio
-import threading
+import time
 
 def TelechargerUneVideo(Lien, Output, Format="MP4"):
     try:
@@ -49,11 +46,11 @@ class Window(Tk):
         self.title=("YouDown")
         self.geometry("740x580")
         self.configure(background="#F3F3F3")
-
+        self.resizable(False,False)
         self.CheminDossier=""
 
 
-        self.listeURL=[] #Contiendra la liste des liens de de la forme [lien,Format,Dossier, Bool] avec Bool sur False tant que le lien n'a pas été converti
+        self.listeURL=[] #Contiendra la liste des liens de de la forme [lien,Format,Dossier, Bool, titre] avec Bool sur False tant que le lien n'a pas été converti
         self.TelechargementLance=False
         self.NombreTelecharges=0#Compte le nombre de videos telechargees
         #Frame des entrées---------------
@@ -90,84 +87,118 @@ class Window(Tk):
         self.radiobutton_mp4.place(x=50,y=330)
         self.radiobutton_mp3.place(x=130,y=330)
 
-        self.BoutonConvert=customtkinter.CTkButton(self.FrameEntrees, text="Telecharger / Ajouter à la liste", corner_radius=15, width=235, height=38, fg_color=("red","#DF0000"), hover_color=("#DF0000","red"), command=self.AppuiBoutonConvert)
-        self.BoutonConvert.place(x=0, y=390)
+        self.BoutonAddToList=customtkinter.CTkButton(self.FrameEntrees, text="Ajouter à la liste", corner_radius=15, width=235, height=38, fg_color=("red","#DF0000"), hover_color=("#DF0000","red"), command=self.AppuiBoutonAddToList)
+        self.BoutonAddToList.place(x=0, y=390)
 
-        self.TextConfirmation=StringVar(value="")
-        self.LabelConfirmation=customtkinter.CTkLabel(self.FrameEntrees, textvariable=self.TextConfirmation,width=235, height=100)
-        self.LabelConfirmation.place(x=0,y=430)
-
-
+        self.TextConfirmationGauche=StringVar(value="")
+        self.LabelConfirmationGauche=customtkinter.CTkLabel(self.FrameEntrees, textvariable=self.TextConfirmationGauche,width=235, height=100)
+        self.LabelConfirmationGauche.place(x=0,y=430)
 
 
-        self.FrameEntrees.place(x=33,y=0)
+
+        self.MasquePartieEntrees=customtkinter.CTkLabel(self.FrameEntrees, text="Veuillez attendre la fin\ndes telechargements pour ajouter\nde nouvelles vidéos",width=235, height=430, fg_color=("#F3F3F3","#494949"))
+        self.FrameEntrees.place(x=75,y=0)
 
 
 
         #Séparateur ---------------------------------------------------
         self.Separateur=customtkinter.CTkFrame(self, width=4, height=412, fg_color=("red","#DF0000"))
-        self.Separateur.place(x=305,y=84)
+        self.Separateur.place(x=380,y=84)
 
         self.ModeBouton= StringVar(value="Light")
         self.BoutonMode=customtkinter.CTkButton(self, textvariable=self.ModeBouton,text_color=("black","#dce4ee"), corner_radius=15, width=45, height=30, fg_color=("#D9D9D9","#252525"), hover_color=("red","#A50000"), command=self.SwitchTheme)
-        self.BoutonMode.place(x=352, y=30)
+        self.BoutonMode.place(x=427, y=30)
 
 
         self.BoutonDoc=customtkinter.CTkButton(self, text="Aide",text_color=("black","#dce4ee"), corner_radius=15, width=45, height=30, fg_color=("#D9D9D9","#252525"), hover_color=("red","#A50000"))
-        self.BoutonDoc.place(x=422, y=30)
+        self.BoutonDoc.place(x=497, y=30)
 
 
-        #PARTIE GAUCHE-------------------------------------------------
+        #PARTIE Droites-------------------------------------------------
         # Création du Treeview
-        self.tree = ttk.Treeview(self, columns=("col1", "col2", "col3"), show="headings")
+        self.tree = ttk.Treeview(self, columns=("col1", "col2", "col3", "col4", "col5"), show="headings")
 
         # Définir les colonnes
         self.tree.heading("col1", text="Titre")
         self.tree.heading("col2", text="Format")
         self.tree.heading("col3", text="Etat")
+        self.tree.heading("col4", text="lien")
+        self.tree.heading("col5", text="Output")
+
         self.tree.column("col1", width=132, anchor="center")
         self.tree.column("col2", width=60, anchor="center")
         self.tree.column("col3", width=75, anchor="center")
+        self.tree.column("col3", width=75, anchor="center")
+        self.tree.column('col4', width=0, stretch=NO)
+        self.tree.column('col5', width=0, stretch=NO)
+
+        self.tree.place(x=427,y=84, height=286)
 
 
-        self.tree.place(x=352,y=84, height=412)
+        self.BoutonConvert=customtkinter.CTkButton(self, text="Telecharger", corner_radius=15, width=235, height=38, fg_color=("red","#DF0000"), hover_color=("#DF0000","red"), command=self.AppuiBoutonConvert)
+        self.BoutonConvert.place(x=443, y=390)
+
+        self.TextConfirmationDroite=StringVar(value="")
+        self.LabelConfirmationDroite=customtkinter.CTkLabel(self, textvariable=self.TextConfirmationDroite,width=235, height=100)
+        self.LabelConfirmationDroite.place(x=443,y=430)
 
 
-    def AppuiBoutonConvert(self):
+    def AppuiBoutonAddToList(self):
         lien=self.EntryLien.get()
         if lien=="" :
-            self.TextConfirmation.set("Veuillez renseigner une vidéo")
-            self.LabelConfirmation.configure(text_color=("red","#DF0000"))
+            self.TextConfirmationGauche.set("Veuillez renseigner une vidéo")
+            self.LabelConfirmationGauche.configure(text_color=("red","#DF0000"))
 
         else :
             titre=RecupTitreVideo(self.EntryLien.get())
             if titre=="Erreur de lien":
-                self.TextConfirmation.set("Il semble y avoir\nune erreur de lien")
-                self.LabelConfirmation.configure(text_color=("red","#DF0000"))
+                self.TextConfirmationGauche.set("Il semble y avoir\nune erreur de lien")
+                self.LabelConfirmationGauche.configure(text_color=("red","#DF0000"))
 
             else:
                 if self.CheminDossier=="":
-                    self.TextConfirmation.set("Veuillez sélectionner un dossier de sortie")
-                    self.LabelConfirmation.configure(text_color=("red","#DF0000"))
+                    self.TextConfirmationGauche.set("Veuillez sélectionner un dossier de sortie")
+                    self.LabelConfirmationGauche.configure(text_color=("red","#DF0000"))
                 else :
                         if any(lien in elements for elements in self.listeURL): #On verifie si le lien est déja dans la liste
-                            self.TextConfirmation.set("Cette vidéo est déja dans la liste")
-                            self.LabelConfirmation.configure(text_color=("red","#DF0000"))
+                            self.TextConfirmationGauche.set("Cette vidéo est déja dans la liste")
+                            self.LabelConfirmationGauche.configure(text_color=("red","#DF0000"))
                         else :
                             Format=['MP4','MP3'][self.radio_var.get()-1]
                             Etat="Attente"
                             Dossier=self.CheminDossier
-                            self.listeURL.append([lien,Format,Dossier,False])
-                            self.tree.insert("", "end", values=(titre, Format, Etat))
+                            self.listeURL.append([lien,Format,Dossier,False,titre])
+                            self.tree.insert("", "end", values=(titre, Format, Etat, lien, Dossier))
                             if len(titre)>25:
-                                self.TextConfirmation.set(titre[:22]+ "...\n a été ajouté à la liste")
-                                self.LabelConfirmation.configure(text_color=("black","#dce4ee"))
+                                self.TextConfirmationGauche.set(titre[:22]+ "...\n a été ajouté à la liste")
+                                self.LabelConfirmationGauche.configure(text_color=("black","#dce4ee"))
                             else :
-                                self.TextConfirmation.set(titre+ "\n a été ajouté à la liste")
-                                self.LabelConfirmation.configure(text_color=("black","#dce4ee"))
+                                self.TextConfirmationGauche.set(titre+ "\n a été ajouté à la liste")
+                                self.LabelConfirmationGauche.configure(text_color=("black","#dce4ee"))
+
+    def AppuiBoutonConvert(self):
+        self.MasquePartieEntrees.place(x=0,y=100)
+        liste_video=[]
+        for Id_video in self.tree.get_children() :
+            liste_video.append([Id_video,self.tree.item(Id_video)['values']]) #On otbient une liste de la forme [Id_video,[Infos Vide], ...]
+        for video in liste_video:
+            Id= video[0]
+            titre= video[1][0]
+            Format= video[1][1]
+            Etat= video[1][2]
+            Lien= video[1][3]
+            output= video[1][4]
+
+        #Mettre fpocus sur celui qui se fait DL
+
+##            titre=video[4]
+##            if len(titre)>25:
+##                self.TextConfirmationDroite.set(titre[:22]+ "...\n en cours de télechargement")
+##            else :
+##                self.TextConfirmationDroite.set(titre+ "\n en cours de télechargement")
 
 
-
+        #self.MasquePartieEntrees.place(x=1000,y=1000)
 
     def ChoisirDossier(self):
         """Cette fonction ouvre l'explorateur de fichier et permet d'ouvrir un dossier en renvoyant son chemin"""
@@ -176,6 +207,7 @@ class Window(Tk):
         self.TextDossierChoisi.set(Nom)
 
     def SwitchTheme(self):
+
         if self.ModeBouton.get()=="Light":
             self.SetLightTheme()
         else :
@@ -205,26 +237,7 @@ class Window(Tk):
         self.SetDarkTheme()
         self.mainloop()
 
-    def TelechargerListe(self):
-        pass
-##        if self.TelechargementLance==False : #Si aucun telechargment n'est en cours
-##            self.TelechargementLance=True#On enregoistre l'etat des telechargement sur True
-##            NbTelecharges=self.NombreTelecharges
-##            NbLien=len(self.listeURL)
-##            while NbTelecharges!= NbLien : #Tant qu'on a pas tout telechargé
-##                for i in range(len(self.listeURL)): #On parcourt la liste jusqu'a trouvé le premier non telechargé
-##                    if self.listeURL[i][3]==False :
-##                        lien=self.listeURL[i][0]
-##                        Format=self.listeURL[i][1]
-##                        Dossier=self.listeURL[i][2]
-##                        TelechargerUneVideo(lien, Dossier, Format)
-##                        print(lien,Format,Dossier)#??
-##                        self.listeURL[i][3]=True
-##                        self.NombreTelecharges+=1
-##                        break
-##                NbTelecharges=self.NombreTelecharges
-##                NbLien=len(self.listeURL)
-##            self.TelechargementLance=False
+
 
 
 Fenetre=Window()
